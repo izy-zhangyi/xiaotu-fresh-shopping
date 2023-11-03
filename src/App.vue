2b2a41fe50020b3ab3e3fd573a661d56066d4815
🔥🔥🔥🔥 <zyzhangyii@163.com>
2023-11-03 17:29:24 +0800

    路由及相关组件页面配置

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup></script>
 
 <template>
+  <!-- 一级路由出口 -->
   <RouterView />
 </template>
 
---
+++ src/views/layout/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <!-- 二级路由出口 -->
+    <RouterView />
+  </div>
+</template>
