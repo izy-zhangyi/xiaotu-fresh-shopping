b4acb4d73ce3d959d33a1113484e3d42a7109679
🔥🔥🔥🔥 <zyzhangyii@163.com>
2023-11-06 18:51:52 +0800

    本地购物车-接口购物车-删除购物车实现&优化

--- src/apis/cart.js
+++ src/apis/cart.js
@@ -5,3 +5,7 @@ export const insertCartAPI = (data) => https.post('/member/cart', data)
 
 // 获取最新的购物车列表数据
 export const findNewCartListAPI = () => https.get('/member/cart')
+
+// 删除购物车
+export const delCartAPI = (ids) =>
+  https.delete('/member/cart', { data: { ids } })
--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,7 +1,7 @@
 import { defineStore } from 'pinia'
 import { ref, computed } from 'vue'
 import { useUserStore } from '@/stores/userStore.js'
-import { insertCartAPI, findNewCartListAPI } from '@/apis/cart.js'
+import { insertCartAPI, findNewCartListAPI, delCartAPI } from '@/apis/cart.js'
 export const useCartStore = defineStore(
   'cart',
   () => {
@@ -39,12 +39,17 @@ export const useCartStore = defineStore(
     }
 
     // 删除购物车-删除全部数据
-    const delCart = (skuId) => {
-      // 思路：
-      // 1. 找到要删除项的下标值 - splice
-      // 2. 使用数组的过滤方法 - filter
-      const idx = cartList.value.findIndex((item) => skuId === item.skuId)
-      cartList.value.splice(idx, 1)
+    const delCart = async (skuId) => {
+      if (isLogin.value) {
+        await delCartAPI([skuId])
+        newCartList()
+      } else {
+        // 思路：
+        // 1. 找到要删除项的下标值 - splice
+        // 2. 使用数组的过滤方法 - filter
+        const idx = cartList.value.findIndex((item) => skuId === item.skuId)
+        cartList.value.splice(idx, 1)
+      }
     }
     // 计算所有商品总数量
     const allCount = computed(() =>
